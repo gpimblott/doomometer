cf76a1ee2785048d8c64277e6fff1117c8880edd
pimblottg <g.pimblott@kainos.com>
2016-03-21 22:42:20 +0000

    Update Virus

--- db/virus.js
+++ db/virus.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var dbpool = require('../config/dbpool');
-
+var dateutil = require ('../utils/DateTime.js');
 
 var Virus = function () {};
 
@@ -30,7 +30,7 @@ Virus.todayTable = function() {
 
                         var row = "<tr>";
                         row += "<td><a href=\"" + item.url + "\" target=\"_blank\">" + item.name + "</a></td>";
-                        row += "<td>" + formatDate( item.time ) + "</td>";
+                        row += "<td>" + dateutil.DDMM_Time( item.time ) + "</td>";
                         row += "<td><a href=\"" + item.url + "\" target=\"_blank\">View</a></td>";
                         row += "</tr>";
 
@@ -65,9 +65,9 @@ Virus.summary = function() {
                     rows.forEach( function (item, index) {
 
                         var row = "<tr>";
-                        row += "<td><a href=\"virus?date=" + formatLinkDate(item.date) + "\">" + formatSummaryDate(item.date) + "</a></td>";
+                        row += "<td><a href=\"virus?date=" + dateutil.formatLinkDate(item.date) + "\">" + dateutil.DDMMYY(item.date) + "</a></td>";
                         row += "<td>" + item.count + "</td>";
-                        row += "<td><a href=\"virus?date=" + formatLinkDate(item.date) + "\">View</a></td>";
+                        row += "<td><a href=\"virus?date=" + dateutil.formatLinkDate(item.date) + "\">View</a></td>";
                         row += "</tr>";
 
                         fileStream.write( row + "\n");
@@ -86,40 +86,5 @@ Virus.summary = function() {
     });
 }
 
-
-function formatDate(d) {
-
-    var hrs = d.getUTCHours();
-    var minute = d.getUTCMinutes();
-
-    if( hrs < 10 ) hrs = '0' + hrs;
-    if( minute < 10) minute = '0' + minute;
-
-    return hrs + ":" + minute + " UTC";
-}
-
-function formatSummaryDate(d) {
-
-    var year = d.getFullYear();
-    var month = d.getMonth()+1;
-    var day = d.getDate();
-
-    if( month < 10 ) month = '0' + month;
-    if( day < 10 ) day = '0' + day;
-
-    return day + "-" + month + "-" + year;
-}
-
-function formatLinkDate(d) {
-
-    var year = d.getFullYear();
-    var month = d.getMonth()+1;
-    var day = d.getDate();
-
-    if( month < 10 ) month = '0' + month;
-    if( day < 10 ) day = '0' + day;
-
-    return year + "-" + month + "-" + day;
-}
 
 module.exports = Virus;
